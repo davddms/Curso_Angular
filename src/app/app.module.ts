cc2a58e3840aa1518bb24398665cd8dc433bee47
chenaoh <davinsonmontealegred@unimagdalena.edu.co>
2023-10-06 15:58:46 -0500

    Curso_Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador.component';
+import { BotonesComponent } from './botones/botones.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { BuclesComponent } from './bucles/bucles.component';
+import { SwitchComponent } from './switch/switch.component';
+import { Formulario2Component } from './formulario2/formulario2.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Formulario3Component } from './formulario3/formulario3.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContadorComponent,
+    BotonesComponent,
+    FormularioComponent,
+    BuclesComponent,
+    SwitchComponent,
+    Formulario2Component,
+    Formulario3Component
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/bucles/bucles.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+import { Persona } from '../persona';
+
+@Component({
+  selector: 'app-bucles',
+  templateUrl: './bucles.component.html',
+  styleUrls: ['./bucles.component.css']
+})
+export class BuclesComponent {
+
+  personas: Persona[] = [
+    {nombre: 'Juan', edad: 20},
+    {nombre: 'Ana', edad: 25},
+    {nombre:'Luis', edad: 30},
+    {nombre: 'Maria', edad: 35},
+  ];
+
+
+
+}
+ 
